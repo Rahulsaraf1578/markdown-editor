cbe70dbc0505cee6cb736f3fc1f15e3d97df9830
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2022-05-31 14:40:53 +0530

    Making basic layout
    We have made some container for the basic layout of the app. For that purpose we have used styled library for making css component for style.

--- markdowneditor/src/App.js
+++ markdowneditor/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+  width:100%;
+  height:100%;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+`;
+
+const Title = styled.div`
+  font-size:25px;
+  font-weight:700;
+  font-family:"Lato", sans-serif;
+  margin-bottom:1em;
+`
+
+const EditorContainer = styled.div`
+  width:100%;
+  height:100%;
+  display:flex;
+`
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppContainer>
+      <Title>Markdown editor</Title>
+      <EditorContainer>
+        
+      </EditorContainer>
+    </AppContainer>
   );
 }
 
